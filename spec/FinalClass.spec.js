200970017ac90acea78e695f2815cb9a851d3776
Szymon Wygnanski <s@finalclass.net>
2013-04-21 10:24:42 +0200

    added possibility to describe simple objects

--- spec/FinalClass.spec.js
+++ spec/FinalClass.spec.js
@@ -14,7 +14,6 @@ describe('FinalClass', function () {
             this.abc = 8;
         }
 
-
         expect(function () {
             ChildClass = new FinalClass({
                 className: 'test_Child',
@@ -40,7 +39,55 @@ describe('FinalClass', function () {
         expect(child.abc).toBe(8);
 
         expect(child instanceof ParentClass).toBe(true);
+    });
 
+    it('can describe simple objects', function () {
+        var obj = {};
+
+        expect(function () {
+            FinalClass.describe(obj, {
+                test: {
+                    value: function () {
+                        return 'test';
+                    }
+                }
+            });
+        }).not.toThrow();
+
+        expect(obj.test()).toBe('test');
+    });
+
+    it('do not overlap', function () {
+        var f1,
+            f2,
+            beforeConstructor = jasmine.createSpy('beforeConstructor'),
+            FClass = new FinalClass({
+            className: 'FClass',
+            parents: [],
+            descriptor: {
+                howMuch: {
+                    on: {
+                        beforeConstructor: function () {
+                            this.howMuch = 10;
+                        }
+                    }
+                },
+                mockTest: {
+                    on: {
+                        beforeConstructor: beforeConstructor
+                    }
+                }
+            }
+        });
+
+        f1 = new FClass();
+        f1.howMuch = 5;
+        f2 = new FClass();
+
+        expect(f2.howMuch).toBe(10);
+        expect(f1.howMuch).toBe(5);
+
+        expect(beforeConstructor.calls.length).toBe(2);
     });
 
 });
